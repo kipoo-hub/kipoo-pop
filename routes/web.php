014bc43d8a6b869e2e05a7b2bac1f7be7032ff96
kipoo <taufiq24si@mahasiswa.pcr.ac.id>
2025-09-22 09:41:24 +0700

    project laravel

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Http\Contollers\MahasiswaController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Contollers\MahasiswaController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -26,8 +27,7 @@ Route::get('/panggilan/{param1?}', function ($param1 = '') {
 Route::get('/mahasiswa/{param1?}', [MahasiswaController::class, 'show'])->name('mahasiswa.show');
 
 Route::get('/about', function () {
-
-
-
     return view('halaman-about');
 });
+
+Route::get('/home', [HomeController::class, 'index']);
