5eb74228adea5d611f2c546014e1ba9c1872578a
kipoo-hub <taufiq24si@mahasisw.pcr.ac.id>
2025-10-13 09:06:03 +0700

    Pembuatan form untuk pelanggan, dan penerapan CRUD

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\PegawaiController;
 use App\Http\Contollers\MahasiswaController;
 use App\Http\Controllers\QuestionController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\PelangganController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -39,15 +40,12 @@ Route::get('/home', [HomeController::class, 'index']);
 Route::get('/pegawai', [PegawaiController::class, 'index']);
 
 Route::post('question/store', [QuestionController::class, 'store'])
-<<<<<<< HEAD
-		->name('question.store');
-
-Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
-=======
     ->name('question.store');
+
 Route::get('/auth', [AuthController::class, 'index']);
 Route::post('/auth/login', [AuthController::class, 'login']);
 Route::get('/dashboard', function () {
     return view('dashboard');
-});
->>>>>>> 0a60a58de70e4e13fcafc81545eda2b86eb00810
+})->name('dashboard');
+
+Route::resource('pelanggan', PelangganController::class);
